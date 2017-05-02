5051d87d988ffd2b48917916aad39dfb617642c6
Karthik Meda <karthikreddymeda@gmail.com>
2017-05-02 22:31:14 +0530

    heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://kmeda:Qwerty123$@ds129031.mlab.com:29031/todoapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoapp');
 
 module.exports = {mongoose};
+
+//'mongodb://kmeda:Qwerty123$@ds129031.mlab.com:29031/todoapp'
